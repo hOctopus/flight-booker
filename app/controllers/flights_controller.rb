68e2f9965e6c66f18c0504b69a7e655e6192dc27
Helena Suess <helenasuess@gmail.com>
2016-03-02 00:53:08 -0600

    Search done

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,11 @@
 class FlightsController < ApplicationController
 
   def index
+    @airport_options = Airport.pluck(:name, :code, :id).sort
+    @date_options    = Flight.all.sort.map { |f| f.start.strftime("%b %d, %Y, %I:%M %p") }
+    @flights         = Flight.search(params)
+    if @flights.none?
+      flash[:danger] = "Sorry, there are no available flights."
+    end
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,17 @@ class Flight < ActiveRecord::Base
 
   belongs_to :origin,      class_name: 'Airport'
   belongs_to :destination, class_name: 'Airport'
+
+  def self.search(params)
+    date = params[:flight_date]
+    date.nil? ? date = Date.today : date = date.to_date
+    Flight.where(origin_id:      params[:from_id],
+                 destination_id: params[:to_id],
+                 start:          date.beginning_of_day..date.end_of_day)
+                .includes(:origin, :destination)
+  end
+
+  def duration_in_hours
+    Time.at(self.duration).utc.strftime("%H hr, %M min")
+  end
 end
